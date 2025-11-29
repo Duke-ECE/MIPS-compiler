648cf27d4cf36e42612d844eb0d1a4c433bab6d3
Hailin Xie <hx101@duke.edu>
2025-11-29 21:55:01 +0800

    对编译器进行了测试

--- docs/test.c
+++ docs/test.c
@@ -5,9 +5,6 @@ int main() {
     x = 3;
     y = x + 5;
 
-    while (y > 0) {
-        y = y - 1;
-    }
 
     return 0;
 }
--- docs/test.s
+++ docs/test.s
@@ -1,22 +1,18 @@
 main:
-addi $r1, $r0, 3
-addi $r2, $r1, 0
-addi $r3, $r0, 5
-add $r4, $r2, $r3
-addi $r5, $r4, 0
-L3:
-addi $r6, $r0, 0
-bgt $r5, $r6, LCMP_T_0
-addi $r7, $r0, 0
-j LCMP_E_0
-LCMP_T_0:
-addi $r7, $r0, 1
-LCMP_E_0:
-beq $r7, $r0, L4
-addi $r8, $r0, 1
-sub $r9, $r5, $r8
-addi $r5, $r9, 0
-j L3
-L4:
-addi $r10, $r0, 0
-jr $r10
+# Function prologue
+addi $sp, $sp, -28
+sw $fp, 1($sp)
+addi $fp, $sp, 0
+sw $ra, 2($sp)
+addi $t0, $zero, 3
+sw $t0, -1($fp)
+addi $t1, $zero, 5
+add $t2, $t0, $t1
+sw $t2, -2($fp)
+addi $t3, $zero, 0
+addi $v0, $t3, 0
+# Function epilogue
+lw $ra, 2($fp)
+lw $fp, 1($fp)
+addi $sp, $sp, 28
+jr $ra
